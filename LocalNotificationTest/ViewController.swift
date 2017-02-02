6a03408496b60fe29bd1ee591db264a0c35c5f64
Kanakaraju Chinnam <kanakarajuchinnam@kanakarajus-macbook-pro.local>
2017-02-01 21:29:00 -0700

    Initial Commit

---
+++ LocalNotificationTest/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  LocalNotificationTest
+//
+//  Created by Kanakaraju Chinnam on 1/25/17.
+//  Copyright © 2017 ABC. All rights reserved.
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UNUserNotificationCenter.current().delegate = self
+    }
+
+    @IBAction func DidTapButton(_ sender: UIButton) {
+        // Request for Notification Settings
+
+        UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
+            switch notificationSettings.authorizationStatus {
+            case .notDetermined:
+
+                self.requestAuthorization(completionHandler: { (success) in
+                    guard success else { return }
+
+                    // Schedule The Local Notification
+
+                    self.scheduleLocalNotification()
+                })
+
+            // Request Authorization
+            case .authorized:
+                self.scheduleLocalNotification()
+
+            // Schedule Local Notification
+            case .denied:
+                print("Application Not Allowed to Display Notifications")
+            }
+        }
+    }
+    // Add Trigger
+    // let notificationtrigger = UNTimeIntervalNotificationTrigger(timeInterval: 3.0, repeats: false)
+
+    // MARK: - Private Methods
+
+    private func requestAuthorization(completionHandler: @escaping (_ success: Bool) -> ()) {
+
+        // Request Authorization
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+            if let error = error {
+                print("Request Authorization Failed (\(error), \(error.localizedDescription))")
+            }
+            completionHandler(success)
+        }
+    }
+    private func scheduleLocalNotification() {
+
+        // Create Notification Content
+        let notificationContent = UNMutableNotificationContent()
+
+        // Configure Notification Content
+        notificationContent.title =  "HP Inc"
+        notificationContent.subtitle = "Connect to Printer"
+        //  notificationContent.badge =
+        notificationContent.body = "Hey! you are entering into HP Inc. You can conect to the available Printer"
+
+        // Add Trigger
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 2.0, repeats: false)
+
+        // Create Notification Request
+        let notificationRequest = UNNotificationRequest(identifier: "HP Inc_local_notification", content: notificationContent, trigger: notificationTrigger)
+
+        // Add Request to User Notification Center
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if let error = error {
+                print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
+            }
+        }
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
